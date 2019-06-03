933ef7a666329d18f4863016be65857ec39353e0
Marc Newton <php@marcnewton.info>
2019-06-03 23:30:02 +0100

    Create ApplicationController.php

---
+++ app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ApplicationController extends Controller
+{
+    /**
+     * Returns HTML markup on static request else return application configuration JSON payload.
+     * Returns application initialization data on app boot.
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\JsonResponse|\Illuminate\View\View
+     */
+    public final function index(Request $request)
+    {
+        if($request->ajax())
+
+            return response()->json([
+                'routes' => User::Routes(),
+                'translations' => User::Language()
+            ]);
+
+        return view('app');
+    }
+
+    /**
+     * Returns JSON payload for the current authenticated user else returns 401 header if guest.
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public final function User(Request $request)
+    {
+        $user = $request->user();
+
+        if($user === null)
+            abort(401);
+
+        return $user;
+    }
+
+}
