dc9530c254459e86243cbc4446d8269f93f2e222
Marc Newton <php@marcnewton.info>
2019-06-03 23:35:57 +0100

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,71 @@
+path = require('path');
+
+const target = 'http://192.168.10.10';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+
+    pwa: {
+        name: 'Laravue SPA',
+        msTileColor: '#4DBA87'
+    },
+
+    baseUrl: undefined,
+    assetsDir: undefined,
+
+    devServer: {
+        disableHostCheck: true,
+        proxy: {
+            '^/api': {
+                target: target,
+                changeOrigin: true,
+                secure: false,
+                ws: true
+            },
+            '^/app': {
+                target: target,
+                changeOrigin: true,
+                secure: false,
+                ws: true
+            },
+            '^/socket.io': {
+                target: target
+            }
+        }
+    },
+
+    outputDir: 'public',
+    runtimeCompiler: true,
+    filenameHashing: true,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    css: undefined,
+
+    chainWebpack: config => {
+
+        config.plugins.delete('copy');
+
+        if(isProduction) {
+
+            config
+                .plugin('html')
+                .tap(args => {
+                    args[0].template = 'src/templates/app.blade.php';
+                    args[0].filename = '../resources/views/app.blade.php';
+                    return args
+                })
+
+        }
+
+    },
+
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '~' : path.resolve(__dirname,'src')
+            }
+        }
+    }
+
+}
