924dc039d197a213a8880eac790f8d19cd0ce2a3
Marc Newton <php@marcnewton.info>
2019-06-05 00:11:38 +0100

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+
+/**
+ * Import global components here.
+ * ----------------------------------------------------------------
+ *
+ *
+ * === Static import example ===
+ *
+ * import MyStaticExample from 'my-static-example'
+ * Vue.component('my-static-example', MyStaticExample);
+ *
+ *
+ * === Asyncronous import example (Code splitting) ===
+ *
+ * const MyAsyncExample = () => import('my-async-example');
+ * Vue.component('my-async-example', MyAsyncExample);
+ */
+ 
+ 
