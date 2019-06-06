40272129ad2106f196df2f2416ff2ff2cadcc257
Marc Newton <php@marcnewton.info>
2019-06-06 21:16:07 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import './scss/app.scss'
+
+/**
+ * Import Vue
+ * https://vuejs.org/
+ */
+import Vue from 'vue'
+Vue.config.productionTip = false;
+
+/**
+ * Import Lodash
+ * https://lodash.com/docs
+ */
+
+import 'lodash'
+Vue.prototype._ = window._;
+
+
+/**
+ * Initialize the main application
+ */
+import App from './App'
+
+window.App = new Vue({
+
+    render: h => h(App),
+    store,
+    router
+
+}).$mount(config.mountAppTo);
