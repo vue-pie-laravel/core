ae74e73bacc555185cfa1914eea5d9831652b418
Marc Newton <php@marcnewton.info>
2019-06-14 22:40:33 +0100

    Add support for plugin life cycle hooks and event hooks.

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,55 @@
 import Vue from 'vue'
 import { plugins } from '~/config'
 
+class VueAfterInit {
+
+    constructor (vm) {
+
+        if(! VueAfterInit.instance ) {
+
+            this.hooks = [];
+
+            VueAfterInit.instanced = false;
+            VueAfterInit.instance = this;
+
+        }
+
+        if(! VueAfterInit.instanced ) {
+
+            VueAfterInit.instanced = vm && vm.hasOwnProperty('_isVue');
+
+            VueAfterInit.instance.hooks.forEach(hook => {
+
+                hook(vm);
+
+            })
+
+        }
+
+        return VueAfterInit.instance;
+
+    }
+
+    registerHook (hook) {
+
+        this.hooks.push(hook);
+
+    }
+
+}
+
+const onAfterInit = (vm) => { return new VueAfterInit(vm) };
 const requirePlugin = require.context('.', true, /(index\.js)/);
+
+Vue.mixin({
+
+   beforeMount() {
+
+       onAfterInit(this);
+
+   }
+
+});
 
 requirePlugin.keys().forEach(source => {
 
@@ -18,6 +66,12 @@ requirePlugin.keys().forEach(source => {
             const enabled = config.enabled == null ? true : config.enabled;
 
             if(enabled) {
+
+                if(plugin.hook) {
+
+                    onAfterInit().registerHook(plugin.hook)
+
+                }
 
                 Vue.use(plugin.default || plugin, config);
 
