e9c3664a60d97de9028c7fe5907ee3d1e9ecd56f
Marc Newton <php@marcnewton.info>
2019-06-14 23:11:55 +0100

    Add Laravel Session Plugin

---
+++ src/plugins/laravel/auth/sessions/index.js
@@ -0,0 +1,40 @@
+import Sessions from './module'
+import store from '~/store'
+
+import { mapActions } from 'vuex'
+
+export const hook = (vm) => {
+
+    vm.$on('initialized', (vm) => {
+
+        vm.authCheck();
+
+    });
+
+};
+
+export default {
+
+    install: (Vue, options = {
+
+    }) => {
+
+        store.registerModule('Laravel/Sessions', Sessions);
+
+        Vue.mixin({
+
+            methods: {
+
+                ...mapActions({
+                    authLogin: 'Laravel/Sessions/login',
+                    authLogout: 'Laravel/Sessions/logout',
+                    authCheck: 'Laravel/Sessions/check'
+                })
+
+            }
+
+        });
+
+    }
+
+};
