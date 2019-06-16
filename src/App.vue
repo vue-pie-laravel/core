69ca703ccf613ce4458d5529d287d191258502f6
Marc Newton <php@marcnewton.info>
2019-06-16 22:24:29 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,54 @@
 <template>
 
-  <div>Hello World</div>
+    <transition mode="out-in">
+        <component :is="layout"/>
+    </transition>
 
 </template>
+
+<script>
+
+    export default {
+
+        name: 'App',
+        
+        computed: {
+
+            meta() {
+                return this.$router.currentRoute.meta;
+            },
+
+            layout() {
+
+                if(this.isOffline)
+                    return 'layout-offline';
+
+                if (this.isInitializing)
+                    return 'layout-initializing';
+                
+                if(this.$root.isAuthenticating)
+                    return 'layout-initializing';
+
+                if (this.isMaintenanceMode)
+                    return 'layout-maintenance';
+
+                let layout = this.meta.layout || 'default';
+
+                // Current route does not require auth, render current layout.
+                if (this.meta.hasOwnProperty('noAuth') && this.meta.noAuth)
+                    return `layout-${layout}`;
+
+                // The route requires auth, if not authenticated then render auth layout. 
+                if (this.isAuthenticated === false)
+                    return 'layout-auth';
+
+                // User is authenticated, render current layout.
+                return `layout-${layout}`;
+
+            }
+
+        }
+
+    }
+
+</script>
