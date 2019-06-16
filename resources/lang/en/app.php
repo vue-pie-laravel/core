3957d5fe9fa5db25fcc819a0a72b154120bbf1c6
Marc Newton <php@marcnewton.info>
2019-06-16 21:59:57 +0100

    Create app language file.

---
+++ resources/lang/en/app.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for the SPA App's basic functionality.
+    |
+    */
+
+    'initializing' => 'Initializing',
+    'authenticating' => 'Authenticating',
+    'offline' => 'Service Unavailable',
+    'retry' => 'Retry'
+
+];
