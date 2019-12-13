8e918625a60b9c04f25f882b0c77a7d09df64550
Marc Newton <php@marcnewton.info>
2019-12-13 16:20:35 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -83,15 +83,59 @@ window.App = new Vue({
     data: () => ({
 
         loading: 0,
-        csrfToken: ''
+        csrfToken: '',
+        snackbar: {
+            display: false,
+            props: {},
+            text: ''
+        }
 
     }),
 
     computed: {
 
+        ...mapState(['user','isAuthenticating','isAuthenticated','isMaintenanceMode','isOffline']),
+
+        ...mapGetters(['isInitializing']),
+
         isLoading() {
 
             return this.loading > 0;
+
+        },
+
+        meta() {
+
+            return this.$router.currentRoute.meta;
+
+        },
+
+        layout() {
+
+            if(this.isOffline)
+                return 'layout-offline';
+
+            if (this.isInitializing)
+                return 'layout-initializing';
+
+            if(this.isAuthenticating)
+                return 'layout-authenticating';
+
+            if (this.isMaintenanceMode)
+                return 'layout-maintenance';
+
+            let layout = this.meta.layout || 'default';
+
+            // Current route does not require auth, render current layout.
+            if (this.meta.hasOwnProperty('noAuth') && this.meta.noAuth)
+                return `layout-${layout}`;
+
+            // The route requires auth, if not authenticated then render auth layout.
+            if (this.isAuthenticated === false)
+                return 'layout-auth';
+
+            // User is authenticated, render current layout.
+            return `layout-${layout}`;
 
         }
 
@@ -108,7 +152,6 @@ window.App = new Vue({
                 this.$emit('app:initialized');
 
             }
-
 
         },
 
