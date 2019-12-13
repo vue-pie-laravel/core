0653955ec3f4d4a8453d56e39d624369ee7aa8f8
Marc Newton <php@marcnewton.info>
2019-12-13 16:14:37 +0000

    Update Session.js

--- src/plugins/laravel/modules/Session.js
+++ src/plugins/laravel/modules/Session.js
@@ -1,120 +1,130 @@
 import {config} from '~/config'
 import router from '~/router'
 
-export default {
+const state = {};
 
-    namespaced: true,
+const mutations = {};
 
-    actions: {
+const actions = {
 
-        login({commit, rootState}, input) {
+    login({commit}, input) {
 
-            commit('SET_AUTHENTICATING', true, {root: true});
+        commit('SET_AUTHENTICATING', true, {root: true});
 
-            return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
 
-                router.app.$http.post(route('login'), input).then(response => {
+            router.app.$http.post(router.app.route('login'), input).then(response => {
 
-                    commit('SET_USER', response.data.user, {root: true});
-                    commit('SET_AUTHENTICATING', false, {root: true});
-                    resolve(response);
-
-                }).catch(error => {
-
-                    //commit('SET_OFFLINE', true, {root: true});
-                    commit('SET_AUTHENTICATING', false, {root: true});
-                    reject(error);
-
-                });
-
-            });
-
-        },
-
-        logout({dispatch}) {
-
-            router.app.$http.post(route('logout')).catch(error => {
-
-                // TODO Handle logout error
-                console.error(error);
-
-            }).finally(() => {
-
-                dispatch('check');
-
-            });
-
-        },
-
-        recover({}, input) {
-
-            return new Promise((resolve, reject) => {
-
-                router.app.$http.post(route('password.email'), {email: input.email})
-                    .then(response => {
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        reject(error);
-                    });
-
-            });
-
-        },
-
-        update({}, input) {
-
-            return new Promise((resolve, reject) => {
-
-                router.app.$http.post(route('password.reset', {token: router.currentRoute.query.token}), {email: input.email}).then(response => {
-
-                    router.push(router.currentRoute.query.redirect || config.redirectTo);
-                    resolve(response);
-
-                }).catch(error => {
-
-                    reject(error);
-
-                });
-
-            });
-
-        },
-
-        check({commit}, vm) {
-
-            commit('SET_AUTHENTICATING', true, {root: true});
-
-            router.app.$http.get(router.app.route('app.user')).then(response => {
-
-                commit('SET_USER', response.data, {root: true});
-
-                const redirect = router.currentRoute.query.hasOwnProperty('redirect') && !!router.currentRoute.query.redirect
-                    ? router.currentRoute.query.redirect : router.currentRoute.path;
-
-                router.push(redirect, () => {
-                    commit('SET_AUTHENTICATING', false, {root: true});
-                });
+                commit('SET_USER', response.data.user, {root: true});
+                commit('SET_AUTHENTICATING', false, {root: true});
+                resolve(response);
 
             }).catch(error => {
 
-                // TODO Handle error response, ignore codes handled by interceptor
-
-            }).finally(() => {
-
+                //commit('SET_OFFLINE', true, {root: true});
                 commit('SET_AUTHENTICATING', false, {root: true});
+                reject(error);
 
             });
 
-        },
+        });
 
-        reset({commit}) {
+    },
 
-            commit('SET_USER', null, {root: true});
-            commit('SET_AUTHENTICATED', false, {root: true});
+    logout({dispatch}) {
 
-        }
+        router.app.$http.post(router.app.route('logout')).catch(error => {
+
+            // TODO Handle logout error
+            console.error(error);
+
+        }).finally(() => {
+
+            dispatch('check');
+
+        });
+
+    },
+
+    recover(_, input) {
+
+        return new Promise((resolve, reject) => {
+
+            router.app.$http.post(router.app.route('password.email'), {email: input.email})
+                .then(response => {
+                    resolve(response);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+
+        });
+
+    },
+
+    update(_, input) {
+
+        return new Promise((resolve, reject) => {
+
+            router.app.$http.post(router.app.route('password.reset', {token: router.currentRoute.query.token}), {email: input.email}).then(response => {
+
+                router.push(router.currentRoute.query.redirect || config.redirectTo);
+                resolve(response);
+
+            }).catch(error => {
+
+                reject(error);
+
+            });
+
+        });
+
+    },
+
+    check({commit}) {
+
+        commit('SET_AUTHENTICATING', true, {root: true});
+
+        router.app.$http.get(router.app.route('app.user')).then(response => {
+
+            commit('SET_USER', response.data, {root: true});
+
+            const redirect = router.currentRoute.query.hasOwnProperty('redirect') && !!router.currentRoute.query.redirect
+                ? router.currentRoute.query.redirect : router.currentRoute.path;
+
+            router.push(redirect, () => {
+                commit('SET_AUTHENTICATING', false, {root: true});
+            });
+
+        }).catch(error => {
+
+            // TODO Handle error response, ignore codes handled by interceptor
+            console.error(error);
+
+        }).finally(() => {
+
+            commit('SET_AUTHENTICATING', false, {root: true});
+
+        });
+
+    },
+
+    reset({commit}) {
+
+        commit('SET_USER', null, {root: true});
+        commit('SET_AUTHENTICATED', false, {root: true});
 
     }
 
 };
+
+const getters = {};
+
+export default {
+    namespaced: true,
+    dynamic: true,
+    state,
+    mutations,
+    actions,
+    getters
+}
