b25d1a30bdd2447b945ac04477221ad9c90d35a4
Marc Newton <php@marcnewton.info>
2020-01-23 10:21:22 +0000

    Update app.blade.php

--- src/templates/app.blade.php
+++ src/templates/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="shortcut icon" href="/favicon.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="shortcut icon" href="/favicon.png" type="image/png">
     <title>{{ config('app.name') }}</title>
 </head>
 <body>
