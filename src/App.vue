83c65cb26bf841cd8c45be38f66a3d4d29d66b28
Marc Newton <hello@marcnewton.co.uk>
2020-03-07 19:48:32 +0000

    Remove vuetify snack notice.

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,6 @@
       <component :is="$root.layout"/>
     </transition>
 
-    <v-snackbar v-model="$root.snackbar.display" v-bind="$root.snackbar.props">
-      <span v-html="$root.snackbar.text"></span>
-      <v-btn dark text @click="$root.snackbar.display = false">Close</v-btn>
-    </v-snackbar>
-
   </v-app>
 
 </template>
