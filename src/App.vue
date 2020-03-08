1a7af5e8ff0f90b54f589737bfd7a136083b7060
Marc Newton <hello@marcnewton.co.uk>
2020-03-08 21:56:01 +0000

    Transition css

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 
   <div id="app" class="inherit" v-cloak :class="$root.layout">
 
-    <component :is="$root.layout"/>
+    <transition name="component-fade" mode="out-in">
+      <component :is="$root.layout"/>
+    </transition>
 
   </div>
 
