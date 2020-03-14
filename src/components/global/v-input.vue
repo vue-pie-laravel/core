b4fb4c617bd8f244cf14a57db892695d0d2a6bbd
Marc Newton <hello@marcnewton.co.uk>
2020-03-14 19:14:54 +0000

    Add input field for demo.

---
+++ src/components/global/v-input.vue
@@ -0,0 +1,77 @@
+<template>
+
+  <label class="v-input" :class="{ 'v-input-error' : hasError }" @click="clear">
+    <input v-model="form.input[name]" :disabled="form.busy" :placeholder="placeholder" :type="type" :name="name"/>
+  </label>
+
+</template>
+
+<script>
+
+import inputMix from '@/components/global/input/vInputMix'
+
+export default {
+
+  name: 'v-input',
+
+  mixins: [inputMix],
+
+  computed: {
+
+    placeholder () {
+      return this.error || this.label
+    }
+
+  }
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+  .v-input {
+
+    display: block;
+    position: relative;
+    width: 100%;
+    margin: 0.5rem;
+    border-radius: 0.25rem;
+    font-size: 0.75rem;
+    letter-spacing: 0.05rem;
+    color: inherit;
+
+    input {
+
+      all: inherit;
+      margin: 0;
+      outline: none;
+      border: 2px solid transparent;
+      background-color: rgba(40, 40, 40, 0.05);
+      padding: 0.5rem 1rem;
+      cursor: text;
+
+      &:disabled {
+        background-color: rgba(99, 107, 111, 0.51);
+        cursor: not-allowed;
+      }
+
+      &:focus {
+        border-color: rgba(250, 250, 250, 0.1);
+      }
+
+      &::placeholder {
+        color: inherit;
+      }
+
+    }
+
+    &-error {
+
+      background-color: #ca2e3f;
+
+    }
+
+  }
+
+</style>
---
+++ src/components/global/vInputMix.js
@@ -0,0 +1,52 @@
+import Form from '@/utility/Form'
+
+export default {
+
+  props: {
+
+    form: {
+      type: Object,
+      required: true,
+      validator: form => {
+        return form instanceof Form
+      }
+    },
+
+    label: {
+      type: String,
+      required: true
+    },
+
+    name: {
+      type: String,
+      required: true
+    },
+
+    type: {
+      type: String,
+      default: () => ('text')
+    }
+
+  },
+
+  computed: {
+
+    error () {
+      return this.hasError ? this.form.errors.get(this.name) : undefined
+    },
+
+    hasError () {
+      return this.form.errors.has(this.name)
+    }
+
+  },
+
+  methods: {
+
+    clear () {
+      this.form.errors.clear(this.name)
+    }
+
+  }
+
+}
