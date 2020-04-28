fcfcdf1aed8fecc6944c07d19e859d90689f23fb
Marc Newton <hello@marcnewton.co.uk>
2020-04-28 15:53:22 +0100

    Set welcome screen index.

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,19 @@
 |
 */
 
-use \Illuminate\Http\Request;
-use \Illuminate\Support\Facades\Auth;
 use \Illuminate\Support\Facades\Route;
 
-/*********************************************
- * Do not place custom routes after this line.
- *
- * Directs all other requests to Vue Router.
- *********************************************/
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/{vue?}','ApplicationController@index')->where('vue', '.*');
+Route::get('/', function () {
+  return view('welcome');
+})->name('index');
